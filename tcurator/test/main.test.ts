bac70a1fc92679543f8508d9350cb6e3da7bc890
fulcanelly <dallasxfulcanelly@gmail.com>
2023-12-03 20:26:29 +0200

    update tests

--- tcurator/test/main.test.ts
+++ tcurator/test/main.test.ts
@@ -1,11 +1,14 @@
 import '../src/neo4j'
 import '../src/models/__relations'
+import { schanChanHandle, spy } from '../src/chanscan'
 
 import { User } from '../src/models/user'
 import { OnlineLog } from '../src/models/online_log'
 import { Neo4jSupportedProperties, NeogmaModel, QueryBuilder, QueryRunner, Where, getTransaction } from "neogma";
 import { neogma } from "../src/neo4j";
 import { QueryResult, RecordShape } from "neo4j-driver";
+import { Channel, ChannelProps } from '../src/models/channel'
+import { ChannelPost } from '../src/models/channel_post'
 
 
 type AnyObj = Record<string, any>
@@ -32,14 +35,14 @@ function rangUUID() {
 
 async function testTransactionsWorking() {
   await neogma.getTransaction(null, async (t) => {
-    await User.findOne({session: t})
+    await User.findOne({ session: t })
     const one = await User.createOne({
       name: undefined,
       user_id: undefined,
       uuid: rangUUID()
-    }, {session: t})
+    }, { session: t })
 
-    await one.delete({session: t})
+    await one.delete({ session: t })
 
     const count = await new QueryBuilder()
       .match({
@@ -50,9 +53,9 @@ async function testTransactionsWorking() {
       .return('count(u) as c')
       .run(t)
 
-      const res = Number(count.records[0].get('c'))
+    const res = Number(count.records[0].get('c'))
 
-    console.log({res, n: Number(res)})
+    console.log({ res, n: Number(res) })
   })
 }
 
@@ -67,11 +70,20 @@ async function deleteAll() {
   await neogma.queryRunner.run(
     "MATCH (u:`OnlineLog`) WHERE u.uuid starts with 'test:' DETACH DELETE u"
   )
+
+  await neogma.queryRunner.run(
+    "MATCH (u:`Channel`) WHERE u.username starts with 'test:' DETACH DELETE u"
+  )
+
+  await neogma.queryRunner.run(
+    "MATCH (u:`ChannelPost`) WHERE u.post_author = 'test:00000000000000000000------------------------------00000000000000009000000000000:test' DETACH DELETE u"
+  )
 }
+
 
 describe('models ::', () => {
 
-  beforeAll(deleteAll)
+  beforeEach(deleteAll)
 
   it('check transactions is working', testTransactionsWorking)
 
@@ -266,3 +278,286 @@ describe('models ::', () => {
 
 
 })
+
+const post_author = 'test:00000000000000000000------------------------------00000000000000009000000000000:test'
+
+const makeMsg = (packet: spy.Packet) => ({
+  content: JSON.stringify(packet)
+})
+
+const chan = {
+  ack() { },
+  nack() { },
+} as any
+
+describe('schanChanHandle', () => {
+
+  beforeEach(deleteAll)
+
+  describe('channel packet sent >', () => {
+    describe('when no channel >', () => {
+      it('should create channel in db', async () => {
+        const packet: spy.Packet = {
+          type: 'channel',
+          id: -123,
+          title: 'Test Channel',
+          username: 'test:channel',
+          date: Date.now(),
+          log_id: 1
+        }
+        const msg = makeMsg(packet);
+
+        await schanChanHandle(chan, msg);
+
+        const result = await Channel.findOne({ where: { id: packet.id } });
+        expect(result).toBeDefined();
+        expect(result!.title).toBe(packet.title);
+        expect(result!.username).toBe(packet.username);
+      });
+    });
+
+    describe('when there is channel >', () => {
+      it('updates existing channel', () => {
+
+      })
+    })
+
+    it('when same entry received, nothing created or updated', async () => {
+      const packet: spy.Packet = {
+        type: 'channel',
+        id: 125,
+        title: 'Duplicate Test Channel',
+        username: 'test:channel',
+        date: Date.now(),
+        log_id: 3
+      };
+
+      const msg = makeMsg(packet);
+
+      await schanChanHandle(chan, msg); // First call
+      await schanChanHandle(chan, msg); // Duplicate call
+
+      const results = await Channel.findMany({ where: { id: packet.id } });
+      expect(results.length).toBe(1); // Ensure only one entry exists
+    });
+  })
+
+  describe('post pocket sent', () => {
+    beforeEach(async () => {
+      const makeChan = (id: number) => makeMsg({
+        type: 'channel',
+        id,
+        title: 'Duplicate Test Channel',
+        username: 'test:channel',
+        date: Date.now(),
+        log_id: 3
+      });
+
+      await schanChanHandle(chan, makeChan(1));
+      await schanChanHandle(chan, makeChan(2));
+    })
+
+    describe('when regualar post', () => {
+      it('creates record in db', async () => {
+        const packet: spy.Packet = {
+          type: 'post',
+          id: 200,
+          grouped_id: undefined,
+          views: 10,
+          post_author,
+          date: Date.now(),
+          channel_id: 1, // Ensure this channel exists in the DB
+          log_id: 4
+        };
+        const msg = makeMsg(packet);
+
+        await schanChanHandle(chan, msg);
+
+        const result = await ChannelPost.findOne({ where: { id: packet.id, channel_id: packet.channel_id } });
+        expect(result).toBeDefined();
+        expect(result!.post_author).toBe(packet.post_author);
+      })
+    })
+
+    describe('when forwarded from channel', () => {
+      it('creates post, froward_from relation, post in that channel, forawrded from channel', async () => {
+        const packet: spy.Packet = {
+          type: 'post',
+          id: 201,
+          grouped_id: undefined,
+          views: 15,
+          post_author,
+          date: Date.now(),
+          channel_id: 1, // Ensure this channel exists in the DB
+          fwd_from_channel: {
+            channel_post_id: 301,
+            channel_id: 2, // This should be a different channel ID
+            post_author,
+            date: Date.now()
+          },
+          log_id: 5
+        };
+        const msg = makeMsg(packet);
+
+        await schanChanHandle(chan, msg);
+
+        const resultPost = await ChannelPost.findOne({ where: { id: packet.id, channel_id: packet.channel_id } });
+        const resultFwdFromPost = await ChannelPost.findOne({ where: { id: packet!.fwd_from_channel!.channel_post_id, channel_id: packet!.fwd_from_channel!.channel_id } });
+
+        const result = await new QueryBuilder()
+          .match({
+            related: [
+              {
+                model: Channel,
+                where: {
+                  id: 1
+                }
+              },
+              Channel.getRelationshipByAlias('posts'),
+              { model: ChannelPost },
+              ChannelPost.getRelationshipByAlias('forwarded_from'),
+              { model: ChannelPost },
+              ChannelPost.getRelationshipByAlias('of_channel'),
+              {
+                model: Channel,
+                identifier: 'ch'
+              }
+            ]
+          })
+          .return('ch')
+          .run(neogma.queryRunner)
+
+        const res = QueryRunner.getResultProperties<ChannelProps>(result, 'ch')
+
+        expect(res.length).toBe(1)
+        expect(res[0].id).toBe(packet.fwd_from_channel?.channel_id)
+
+        expect(resultPost).toBeDefined();
+        expect(resultPost!.id).toBe(packet.id)
+
+        expect(resultFwdFromPost).toBeDefined();
+        expect(resultFwdFromPost!.id).toBe(packet.fwd_from_channel!.channel_post_id)
+      })
+    })
+
+    describe('when forwarded from user', () => {
+      it('creates post, froward_from relation, user if not exists', async () => {
+        const packet: spy.Packet = {
+          type: 'post',
+          id: 202,
+          grouped_id: undefined,
+          views: 20,
+          post_author,
+          date: Date.now(),
+          channel_id: 1, // Ensure this channel exists in the DB
+          fwd_from_user: {
+            date: Date.now(),
+            user_id: 3
+          },
+          log_id: 6
+        };
+        const msg = makeMsg(packet);
+
+        //NOT SURE ABOUT THIS LINE
+        await (await User.findOne({ where: { user_id: packet!.fwd_from_user!.user_id.toString() } }))?.delete()
+
+        await schanChanHandle(chan, msg);
+
+        const resultPost = await ChannelPost.findOne({ where: { id: packet.id, channel_id: packet.channel_id } });
+        const resultUser = await User.findOne({ where: { user_id: packet!.fwd_from_user!.user_id.toString() } });
+
+        expect(resultPost).toBeDefined();
+        expect(resultUser).toBeDefined();
+      })
+    })
+
+    describe('when regular post already exists', () => {
+      it('nothing created or updated', async () => {
+        const packet: spy.Packet = {
+          type: 'post',
+          id: 200,
+          grouped_id: undefined,
+          views: 10,
+          post_author,
+          date: Date.now(),
+          channel_id: 1, // Ensure this channel exists in the DB
+          log_id: 4
+        };
+        const msg = makeMsg(packet);
+
+        await schanChanHandle(chan, msg);
+        await schanChanHandle(chan, msg);
+
+        const result = await ChannelPost.findMany({ where: { id: packet.id, channel_id: packet.channel_id } });
+
+        expect(result.length).toBe(1);
+      })
+    })
+
+    describe('when forwarded from channel already exists', () => {
+      it('nothing created or updated', async () => {
+        const packet: spy.Packet = {
+          type: 'post',
+          id: 201,
+          grouped_id: undefined,
+          views: 15,
+          post_author,
+          date: Date.now(),
+          channel_id: 1, // Ensure this channel exists in the DB
+          fwd_from_channel: {
+            channel_post_id: 301,
+            channel_id: 2, // This should be a different channel ID
+            post_author,
+            date: Date.now()
+          },
+          log_id: 5
+        };
+        const msg = makeMsg(packet);
+
+        await schanChanHandle(chan, msg);
+        await schanChanHandle(chan, msg);
+
+        const resultPost = await ChannelPost.findMany({ where: { id: packet.id, channel_id: packet.channel_id } });
+        const resultFwdFromPost = await ChannelPost.findMany({ where: { id: packet!.fwd_from_channel!.channel_post_id, channel_id: packet!.fwd_from_channel!.channel_id } });
+
+        expect(resultPost.length).toBe(1);
+        expect(resultFwdFromPost.length).toBe(1);
+      })
+    })
+
+    describe('when forwarded from user already exists', () => {
+      it('nothing created or updated', async () => {
+        const packet: spy.Packet = {
+          type: 'post',
+          id: 202,
+          grouped_id: undefined,
+          views: 20,
+          post_author,
+          date: Date.now(),
+          channel_id: 1, // Ensure this channel exists in the DB
+          fwd_from_user: {
+            date: Date.now(),
+            user_id: 4
+          },
+          log_id: 6
+        };
+        const msg = makeMsg(packet);
+
+        //NOT SURE ABOUT THIS LINE
+
+        await (await User.findOne({ where: { user_id: packet!.fwd_from_user!.user_id.toString() } }))?.delete()
+
+        await schanChanHandle(chan, msg);
+        await schanChanHandle(chan, msg);
+
+        const resultPost = await ChannelPost.findMany({ where: { id: packet.id, channel_id: packet.channel_id } });
+        const resultUser = await User.findMany({ where: { user_id: packet!.fwd_from_user!.user_id.toString() } });
+
+        expect(resultPost.length).toBe(1);
+        expect(resultUser.length).toBe(1);
+      })
+    })
+  })
+
+})
+
