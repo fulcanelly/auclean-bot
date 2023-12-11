29841407ef83a5f50a439aba915e73dacc342a82
fulcanelly <dallasxfulcanelly@gmail.com>
2023-12-11 22:04:35 +0200

    add chan_scan_log#getSession()

--- tcurator/src/models/channel_scan_log_.ts
+++ tcurator/src/models/channel_scan_log_.ts
@@ -1,5 +1,8 @@
 import { ChannelScanLog, ChannelScanLogInstance, ChannelScanLogProps, ChannelScanLogRelatedNodesI } from "./channel_scan_log"
 import { baseInstanceMethods, baseStaticMethods } from "./__base"
+import { QueryBuilder, QueryRunner } from "neogma";
+import { Session, SessionProps } from "./session";
+import { neogma } from "../neo4j";
 
 export const channelScanLogStaticMethods = {
     ...baseStaticMethods
@@ -8,5 +11,33 @@ export const channelScanLogInstanceMethods = {
     ...baseInstanceMethods,
     self() {
         return this as any as ChannelScanLogInstance
+    },
+
+    async getSession() {
+        const queryResult = await new QueryBuilder()
+            .match({
+                related: [
+                    {
+                        model: ChannelScanLog,
+                        where: {
+                            uuid: this.self()!.uuid
+                        }
+                    },
+                    ChannelScanLog.getRelationshipByAlias('handled_by'),
+                    {
+                        model: Session,
+                        identifier: 's'
+                    }
+                ]
+            })
+            .return('s')
+            .run(neogma.queryRunner);
+
+        return Session.buildFromRecord({
+            properties: QueryRunner.getResultProperties<SessionProps>(queryResult, 's')[0],
+            labels: [
+                Session.getLabel()
+            ],
+        });
     }
 };
