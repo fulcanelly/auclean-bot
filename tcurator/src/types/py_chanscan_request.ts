1f83e4820cf2b673e48d8c2e5cdc50fafc7cb610
fulcanelly <dallasxfulcanelly@gmail.com>
2023-12-25 00:16:05 +0200

    update

--- tcurator/src/types/py_chanscan_request.ts
+++ tcurator/src/types/py_chanscan_request.ts
@@ -5,13 +5,16 @@ export type needs_target = { identifier: string }
 export type Type<T> = { type: T }
 
 
+export type job_requests
+	= Type<'full_scan'> & needs_target
+	| Type<'remove_job'>
+	| Type<'test_load'>
+	| Type<'recent_scan'> & needs_target
+
+
 export type py_chanscan_request
 	= requires_session
 	& { log_id?: string; }
-	& ((Type<'full_scan'> & needs_target)
-		| Type<'remove_job'>
-		| Type<'test_load'>
-		| (Type<'recent_scan'> & needs_target));
-
+	& job_requests
 
 
