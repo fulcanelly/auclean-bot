e0d271117dc9a84372579a07d6d4601b17ee437f
fulcanelly <dallasxfulcanelly@gmail.com>
2023-12-30 01:58:07 +0200

    hotfix: fix python

--- python/src/rmq/recv/curator_handler.py
+++ python/src/rmq/recv/curator_handler.py
@@ -27,31 +27,35 @@ def obtain_curator_handler(channel: BlockingChannel):
         print('get someting')
         print(data)
 
+        ch.basic_ack(method.delivery_tag)
+
         if not len(data):
             # TODO, maybe somhow limit it for happening only once per runtime
             print("requesting sessions")
             curator_notifier.request_sessions()
 
-        if sessions:
-            for entry in sessions:
-                session_name = entry.get('session_name')
-                type = entry.get('type')
+        if not sessions:
+            return
 
-                if get_session_store().get(session_name): continue
+        for entry in sessions:
+            session_name = entry.get('session_name')
+            type = entry.get('type')
 
+            if not session_name: continue
+            if not type: continue
+            if get_session_store().get(session_name): continue
 
-                print(os.path.exists(filename_from_session_name(session_name)))
+            print(os.path.exists(filename_from_session_name(session_name)))
 
-                if not os.path.exists(filename_from_session_name(session_name)):
-                    print("Ignoring session")
-                    continue
+            if not os.path.exists(filename_from_session_name(session_name)):
+                print("Ignoring session")
+                continue
 
-                elif type in SESSION_HANDLER_TYPES:
+            elif type in SESSION_HANDLER_TYPES:
 
-                    handler = SESSION_HANDLER_TYPES[type]
-                    get_session_store()[session_name] = handler(session_name, entry.get('user_id'))
-                    get_session_store()[session_name].start()
+                handler = SESSION_HANDLER_TYPES[type]
+                get_session_store()[session_name] = handler(session_name, entry.get('user_id'))
+                get_session_store()[session_name].start()
 
-        ch.basic_ack(method.delivery_tag)
 
     return handle_curator_command
